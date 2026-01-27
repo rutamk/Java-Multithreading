7dbd8800dd0d1030104714160cb7f9b39015f58a
rutamk <rutam2003@gmail.com>
2026-01-28 00:29:24 +0530

    jan 28

--- src/example2/Main.java
+++ src/example2/Main.java
@@ -98,3 +98,70 @@ public class Main {
         }
     }
 }
+//
+//Thread Creation - MultiExecutor Solution
+//In this exercise we are going to implement a  MultiExecutor .
+//
+//The client of this class will create a list of Runnable tasks and provide that list into MultiExecutor's constructor.
+//
+//When the client runs the . executeAll(),  the MultiExecutor,  will execute all the given tasks.
+//
+//To take full advantage of our multicore CPU, we would like the MultiExecutor to execute all the tasks in parallel, by passing each task to a different thread.
+//
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//public class MultiExecutor {
+//
+//    private final List<Runnable> tasks;
+//
+//    /*
+//     * @param tasks to executed concurrently
+//     */
+//    public MultiExecutor(List<Runnable> tasks) {
+//        this.tasks = tasks;
+//    }
+//
+//    /**
+//     * Executes all the tasks concurrently
+//     */
+//    public void executeAll() {
+//        List<Thread> threads = new ArrayList<>(tasks.size());
+//
+//        for (Runnable task : tasks) {
+//            Thread thread = new Thread(task);
+//            threads.add(thread);
+//        }
+//
+//        for(Thread thread : threads) {
+//            thread.start();
+//        }
+//    }
+//}
+
+//import java.util.List;
+//
+//public class MultiExecutor {
+//
+//    // Add any necessary member variables here
+//    List<Runnable> tasks;
+//    /*
+//     * @param tasks to executed concurrently
+//     */
+//    public MultiExecutor(List<Runnable> tasks) {
+//        // Complete your code here
+//        this.tasks = tasks;
+//    }
+//
+//    /**
+//     * Starts and executes all the tasks concurrently
+//     */
+//    public void executeAll() {
+//        // complete your code here
+//        for(Runnable task : tasks){
+//            Thread t = new Thread(task);
+//            t.start();
+//        }
+//    }
+//}
